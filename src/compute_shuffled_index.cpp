c1cb72af45b856c1d4cdc2475ec844a47ccb98d9
Hristo Staykov <izo8611@gmail.com>
2023-10-13 11:55:43 +0300

    Extract utility functions to a header file.

--- src/compute_shuffled_index.cpp
+++ src/compute_shuffled_index.cpp
@@ -4,96 +4,20 @@
 #include <algorithm>
 #include <array>
 
-using namespace nil::crypto3;
+#include "byte_utils.h"
 
-using uint64 = unsigned long long;
-using uint32 = unsigned int;
+using namespace byte_utils;
 
-static const unsigned char SHUFFLE_ROUND_COUNT = 90;
+constexpr unsigned char SHUFFLE_ROUND_COUNT = 90;
 
-#ifdef __ZKLLVM__
-#define assert_true(c) {                 \
-    __builtin_assigner_exit_check(c);    \
-}
-#else
-#define assert_true(c) {                 \
-    assert(c);                           \
-}
-#endif
-
-bool is_same(typename hashes::sha2<256>::block_type block0,
-    typename hashes::sha2<256>::block_type block1){
-
-    bool result = true;
-    for(auto i = 0; i < sizeof(block0)/sizeof(block0[0]) && result; i++) {
-        printf("Element fount %d\n", i);
-        result = result && (block0[0] == block1[0]);
-    }
-
-    return result;
-}
-
-template <typename T>
-char get_nth_byte(const T& val, unsigned int n) {
-    static_assert(std::is_integral<typename std::remove_reference<T>::type>::value, "T must be integral");
-    assert_true(n < sizeof(T));
-    
-    return val >> (n * 8);
-}
-
-template <typename T>
-void sha256_to_bytes_array(typename hashes::sha2<256>::block_type sha, T& out) {
-    assert_true(out.size() >= sizeof(sha));
-    for(int int_count = 0; int_count < sizeof(sha)/sizeof(sha[0]); int_count++) {
-
-        for(int byte_count = 0; byte_count < sizeof(sha[0]); byte_count++) {
-            out[int_count * sizeof(sha[0]) + byte_count] = get_nth_byte<decltype(sha[int_count])>(sha[int_count], byte_count);
-        }
-
-    }
-}
-
-template <typename T, std::size_t inCount, std::size_t N>
-std::array<T, N> take_n_elements(const std::array<T, inCount>& val) {
-    static_assert(N <= inCount);
-    std::array<T, N> ret{};
-    for(auto i = 0u; i < N; i++) {
-        ret[i] = val[i];
-    }
-    return ret;
-}
-
-template <typename T>
-std::array<unsigned char, sizeof(T)> int_to_bytes(const T& paramInt)
-{
-    static_assert(std::is_integral<typename std::remove_reference<T>::type>::value, "T must be integral");
-    std::array<unsigned char, sizeof(T)> arrayOfByte{};
-    for (int i = 0; i < sizeof(T); i++) {
-        arrayOfByte[sizeof(T) - 1 - i] = get_nth_byte(paramInt, i);
-    }
-    return arrayOfByte;
-}
-
-template <typename T>
-T bytes_to_int(const std::array<unsigned char, sizeof(T)>& paramVec)
-{
-    static_assert(std::is_integral<typename std::remove_reference<T>::type>::value, "T must be integral");
-    T val = 0;
-    for (int i = sizeof(T) - 1; i >= 0; i--) {
-        int temp = paramVec[i];
-        val |= (temp << ((sizeof(T) - 1 - i) * 8));
-    }
-    return val;
-}
-
-[[circuit]] uint64 compute_shuffled_index(
-        uint64 index,
-        uint64 index_count,
-        typename hashes::sha2<256>::block_type seed) {
+[[circuit]] uint64_t compute_shuffled_index(
+        uint64_t index,
+        uint64_t index_count,
+        sha256_t seed) {
     assert_true(index < index_count);
 
     std::array<unsigned char, 32+1+4> source_buffer;
-    uint64 cur_idx_permuted = index;
+    uint64_t cur_idx_permuted = index;
 
     sha256_to_bytes_array(seed, source_buffer);
 
@@ -106,12 +30,12 @@ T bytes_to_int(const std::array<unsigned char, sizeof(T)>& paramVec)
         std::array<unsigned char, 32> eth2digest_bytes;
         sha256_to_bytes_array(eth2digest, eth2digest_bytes);
         auto first8bytes = take_n_elements<unsigned char, eth2digest_bytes.size(), 8>(eth2digest_bytes);
-        auto first8bytes_int = bytes_to_int<uint64>(first8bytes);
+        auto first8bytes_int = bytes_to_int<uint64_t>(first8bytes);
         auto pivot = first8bytes_int % index_count;
         auto flip = ((index_count + pivot) - cur_idx_permuted) % index_count;
         auto position = std::max(cur_idx_permuted, flip);
 
-        auto source_buffer_additional_bytes = int_to_bytes(uint32(position >> 8));
+        auto source_buffer_additional_bytes = int_to_bytes(uint32_t(position >> 8));
         for (auto i = 0; i <= 4; i++) {
             source_buffer[33 + i] = source_buffer_additional_bytes[i];
         }
